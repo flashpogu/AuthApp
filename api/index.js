95cf8b3125186493cc9e97a4351c290067d69873
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-03 16:25:00 +0530

    create register api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 mongoose
@@ -8,6 +10,9 @@ mongoose
   .then(() => console.log("Database connect successfully!"))
   .catch((err) => console.log(err));
 const app = express();
+app.use(express.json());
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
