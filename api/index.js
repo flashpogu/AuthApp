28b1f14732169679e8d221019bdba17cf6867101
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-03 16:33:55 +0530

    added middleware func to handle error

--- api/index.js
+++ api/index.js
@@ -14,6 +14,17 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+// Middleware for handling error
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
